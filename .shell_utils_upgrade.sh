a4db1101d98d897be437ad0c2cac7bd6227a95d8
felipefacundes <felipe.facundes@gmail.com>
2025-02-23 16:23:33 -0300

    A Dynamic Collection of Shell Scripts with Educational Purpose

--- .shell_utils_upgrade.sh
+++ .shell_utils_upgrade.sh
@@ -3,3 +3,10 @@ cd ~/.shell_utils || exit
 git add .
 git commit -m "A Dynamic Collection of Shell Scripts with Educational Purpose"
 git push
+
+cat <<'EOF'
+cd ~/.shell_utils || exit
+git add .
+git commit -m "A Dynamic Collection of Shell Scripts with Educational Purpose"
+git push
+EOF
