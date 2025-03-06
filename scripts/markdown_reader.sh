f4d9547275365ddfed4ade25cc1d252e5ca2d65c
felipefacundes <felipe.facundes@gmail.com>
2025-03-05 22:33:46 -0300

    A Dynamic Collection of Shell Scripts with Educational Purpose

--- scripts/markdown_reader.sh
+++ scripts/markdown_reader.sh
@@ -23,55 +23,76 @@ Capacities:
 DOCUMENTATION
 
 # Capture Ctrl+C and end children processes
-#trap '(kill -TERM -- -$$) &>/dev/null' INT TERM SIGHUP SIGQUIT SIGABRT SIGALRM
-trap '(kill -TERM -- -$$) &>/dev/null' SIGINT SIGTERM SIGHUP SIGQUIT #EXIT
+trap '(kill -9 -- -$$) &>/dev/null' SIGINT SIGHUP SIGQUIT #EXIT
 
-# Color definitions with fallback for TTY sessions
-if [[ "${XDG_SESSION_TYPE}" != [Tt][Tt][Yy] ]]; then
-    readonly COLOR_TITLE1='\033[1;38;2;255;128;0;48;2;40;40;40m'        # Orange text on dark gray
-    readonly COLOR_TITLE2='\033[1;38;2;255;192;0;48;2;35;35;35m'        # Yellow text on dark gray
-    readonly COLOR_TITLE3='\033[1;38;2;0;255;127;48;2;30;30;30m'        # Spring green on dark gray
-    readonly COLOR_TITLE4='\033[1;38;2;135;206;250;48;2;25;25;25m'      # Light blue on dark gray
-    readonly COLOR_TITLE5='\033[1;38;2;255;105;180;48;2;20;20;20m'      # Hot pink on dark gray
-    readonly COLOR_TITLE6='\033[1;38;2;147;112;219;48;2;15;15;15m'      # Purple on dark gray
-    #readonly COLOR_CODE='\033[1;38;2;173;216;230m'                     # Light blue for inline code
-    #readonly COLOR_CODE='\033[38;2;212;229;16;48;2;114;99;96m'               
-    readonly COLOR_CODE='\033[38;2;129;250;72;48;2;108;94;87m'               
-    #readonly COLOR_BULLET='\033[1;38;2;50;205;50m'                     # Lime green for bullets
-    readonly COLOR_BULLET='\033[1;3;38;2;201;215;176m'                      
-    readonly COLOR_TABLE_HEADER='\033[1;38;2;0;191;255;48;2;50;50;50m'  # Sky blue for table headers
-    readonly COLOR_LINK='\033[1;38;2;0;255;255m'                        # Cyan for links
-    readonly GRAY='\033[1;38;2;156;156;156m'                            # Fill line
-    readonly COLOR_BOLD='\033[1;38;2;255;215;0m'
-    #readonly INLINE='\033[38;2;0;243;255;48;2;14;69;77m'
-else
-    readonly COLOR_TITLE1='\033[1;38;5;214;48;2;40;40;40m'              # Orange text on dark gray
-    readonly COLOR_TITLE2='\033[1;38;5;226;48;2;35;35;35m'              # Yellow text on dark gray
-    readonly COLOR_TITLE3='\033[1;38;5;82;48;2;30;30;30m'               # Spring green on dark gray
-    readonly COLOR_TITLE4='\033[1;38;5;153;48;2;25;25;25m'              # Light blue on dark gray
-    readonly COLOR_TITLE5='\033[1;38;5;213;48;2;20;20;20m'              # Hot pink on dark gray
-    readonly COLOR_TITLE6='\033[1;38;5;99;48;2;15;15;15m'               # Purple on dark gray
-    #readonly COLOR_CODE='\033[1;38;5;153m'                             # Light blue for inline code
-    #readonly COLOR_CODE='\033[38;5;184;48;5;102m'                        
-    readonly COLOR_CODE='\033[38;5;155;48;5;241m'                        
-    #readonly COLOR_BULLET='\033[1;38;5;46m'                            # Lime green for bullets
-    readonly COLOR_BULLET='\033[1;3;38;5;187m'                             
-    readonly COLOR_TABLE_HEADER='\033[1;38;5;81;48;2;50;50;50m'         # Sky blue for table headers
-    readonly COLOR_LINK='\033[1;38;5;51m'                               # Cyan for links
-    readonly GRAY='\033[1;38;5;244m'                                    # Fill line
-    readonly COLOR_BOLD='\033[1;38;5;220m'
-    #readonly INLINE='\033[38;5;51;48;5;24m'
-fi
+define_colors() {
+    # Color definitions with fallback for TTY sessions
+    if [[ "${NO_COLOR}" == 1 ]]; then
+        readonly COLOR_TITLE1='\033[1m'        
+        readonly COLOR_TITLE2='\033[1m'       
+        readonly COLOR_TITLE3='\033[0m'        
+        readonly COLOR_TITLE4='\033[0m'     
+        readonly COLOR_TITLE5='\033[0m'     
+        readonly COLOR_TITLE6='\033[0m'                  
+        readonly COLOR_CODE='\033[0m'               
+        readonly COLOR_BULLET='\033[1;3m'                      
+        readonly COLOR_TABLE_HEADER='\033[0m'  
+        readonly COLOR_LINK='\033[0m'                      
+        readonly GRAY='\033[1m'    
+        readonly COLOR_BOLD='\033[1m'
+    elif [[ "${XDG_SESSION_TYPE}" != [Tt][Tt][Yy] ]]; then
+        readonly COLOR_TITLE1='\033[1;38;2;255;128;0;48;2;40;40;40m'        # Orange text on dark gray
+        readonly COLOR_TITLE2='\033[1;38;2;255;192;0;48;2;35;35;35m'        # Yellow text on dark gray
+        readonly COLOR_TITLE3='\033[1;38;2;0;255;127;48;2;30;30;30m'        # Spring green on dark gray
+        readonly COLOR_TITLE4='\033[1;38;2;135;206;250;48;2;25;25;25m'      # Light blue on dark gray
+        readonly COLOR_TITLE5='\033[1;38;2;255;105;180;48;2;20;20;20m'      # Hot pink on dark gray
+        readonly COLOR_TITLE6='\033[1;38;2;147;112;219;48;2;15;15;15m'      # Purple on dark gray
+        #readonly COLOR_CODE='\033[1;38;2;173;216;230m'                     # Light blue for inline code
+        #readonly COLOR_CODE='\033[38;2;212;229;16;48;2;114;99;96m'               
+        readonly COLOR_CODE='\033[38;2;129;250;72;48;2;108;94;87m'               
+        #readonly COLOR_BULLET='\033[1;38;2;50;205;50m'                     # Lime green for bullets
+        readonly COLOR_BULLET='\033[1;3;38;2;201;215;176m'                      
+        readonly COLOR_TABLE_HEADER='\033[1;38;2;0;191;255;48;2;50;50;50m'  # Sky blue for table headers
+        readonly COLOR_LINK='\033[1;38;2;0;255;255m'                        # Cyan for links
+        readonly GRAY='\033[1;38;2;156;156;156m'                            # Fill line
+        readonly COLOR_BOLD='\033[1;38;2;255;215;0m'
+        #readonly INLINE='\033[38;2;0;243;255;48;2;14;69;77m'
+    else
+        readonly COLOR_TITLE1='\033[1;38;5;214;48;2;40;40;40m'              # Orange text on dark gray
+        readonly COLOR_TITLE2='\033[1;38;5;226;48;2;35;35;35m'              # Yellow text on dark gray
+        readonly COLOR_TITLE3='\033[1;38;5;82;48;2;30;30;30m'               # Spring green on dark gray
+        readonly COLOR_TITLE4='\033[1;38;5;153;48;2;25;25;25m'              # Light blue on dark gray
+        readonly COLOR_TITLE5='\033[1;38;5;213;48;2;20;20;20m'              # Hot pink on dark gray
+        readonly COLOR_TITLE6='\033[1;38;5;99;48;2;15;15;15m'               # Purple on dark gray
+        #readonly COLOR_CODE='\033[1;38;5;153m'                             # Light blue for inline code
+        #readonly COLOR_CODE='\033[38;5;184;48;5;102m'                        
+        readonly COLOR_CODE='\033[38;5;155;48;5;241m'                        
+        #readonly COLOR_BULLET='\033[1;38;5;46m'                            # Lime green for bullets
+        readonly COLOR_BULLET='\033[1;3;38;5;187m'                             
+        readonly COLOR_TABLE_HEADER='\033[1;38;5;81;48;2;50;50;50m'         # Sky blue for table headers
+        readonly COLOR_LINK='\033[1;38;5;51m'                               # Cyan for links
+        readonly GRAY='\033[1;38;5;244m'                                    # Fill line
+        readonly COLOR_BOLD='\033[1;38;5;220m'
+        #readonly INLINE='\033[38;5;51;48;5;24m'
+    fi
 
-readonly RED='\033[1;31m'                                               # Red color
-readonly YELLOW='\033[1;33m'                                            # Yellow color
-readonly UNDERLINE='\033[1;4;38;5;187m'                                 # Underline
-readonly COLOR_RESET='\033[0m'                                          # Reset color
+    if [[ "${NO_COLOR}" == 1 ]]; then
+        readonly RED='\033[0m'                                               
+        readonly YELLOW='\033[0m'                                            
+        readonly UNDERLINE='\033[1;4m'                                 
+    else
+        readonly RED='\033[1;31m'                                               # Red color
+        readonly YELLOW='\033[1;33m'                                            # Yellow color
+        readonly UNDERLINE='\033[1;4;38;5;187m'                                 # Underline
+    fi
+    readonly COLOR_RESET='\033[0m'                                          # Reset color
+}
 
-declare -A MESSAGES
 
 # Check the system language and assign messages accordingly
 update_variables() {
+    declare -A MESSAGES
+
     if [[ "${LANG,,}" =~ pt_ ]]; then
         MESSAGES=(
             ["please_install"]="${RED}Erro: Dependências ausentes: ${YELLOW}${missing_deps[*]}\n${RED}Por favor, instale os pacotes necessários e tente novamente.${COLOR_RESET}"
@@ -88,6 +109,9 @@ Opções:
 -nl, --no-less         Desativar o modo de paginação com less
 -nh, --no-hl           Desativar realce de sintaxe para blocos de código
 -ic, --invert-color    Ativa a inversão de cores
+-nc, --no-centralize   Desativa a centralização de texto
+-nf, --no-fill         Desativa o preenchimento de fundo de linha
+-s,--simple            Desativa as cores, a centralização de texto e o preenchimento de fundo de linha
 
 Exemplos:
 ${0##*/} documento.md
@@ -118,15 +142,18 @@ Uso: ${0##*/} [OPCIONES] archivo
 Opciones:
 -h, --help             Mostrar este mensaje de ayuda
 -nl, --no-less         Desactivar el modo de paginación con less
--nh, --no-hl           Desactivar resaltado de sintaxis para bloques de código
--ic, --invert-color    Activa la inversión de colores
+-nh, --no-hl           Desactivar el resaltado de sintaxis para bloques de código
+-ic, --invert-color    Activar la inversión de colores
+-nc, --no-centralize   Desactivar la centralización del texto
+-nf, --no-fill         Desactivar el relleno del fondo de las líneas
+-s, --simple           Desactivar los colores, la centralización del texto y el relleno del fondo de las líneas
 
 Ejemplos:
 ${0##*/} documento.md
 ${0##*/} --no-hl README.md
 
-Características de Markdown compatibles:
-• Encabezados (niveles 1-6)
+Características de Markdown soportadas:
+• Títulos (niveles 1-6)
 • Bloques de código con resaltado de sintaxis
 • Código en línea
 • Listas no ordenadas
@@ -143,28 +170,31 @@ EOF
             ["no_input"]="${RED}Error: No input file specified${COLOR_RESET}\n"
             ["help"]=$(
         cat << EOF
-Markdown Reader - A comprehensive markdown parser and formatter
+Markdown Reader - A complete Markdown parser and formatter
 
 Usage: ${0##*/} [OPTIONS] file
 
 Options:
--h, --help             Show this help message
--nl, --no-less         Disable pager mode with less
+-h, --help             Display this help message
+-nl, --no-less         Disable pagination mode with less
 -nh, --no-hl           Disable syntax highlighting for code blocks
--ic, --invert-color    Enables color inversion
+-ic, --invert-color    Enable color inversion
+-nc, --no-centralize   Disable text centering
+-nf, --no-fill         Disable background filling of lines
+-s, --simple           Disable colors, text centering, and background filling of lines
 
 Examples:
 ${0##*/} document.md
 ${0##*/} --no-hl README.md
 
-Supported Markdown Features:
-• Headers (level 1-6)
+Supported Markdown features:
+• Headings (levels 1-6)
 • Code blocks with syntax highlighting
 • Inline code
 • Unordered lists
 • Horizontal rules
 • HTML line breaks
-• HTML underline and more
+• HTML underlining and more
 EOF
             )
         )
@@ -400,20 +430,22 @@ process_markdown() {
         # Handle Titles (Headers)
         if [[ "$line" =~ ^######[^#](.+)$ ]]; then
             detect_first_color
-            line=$(echo -e "${COLOR_TITLE6}${BASH_REMATCH[1]}${first_color}")   # Title level 6
+            line=$(echo -e "${COLOR_TITLE6}${BASH_REMATCH[1]}${first_color}")
         elif [[ "$line" =~ ^#####[^#](.+)$ ]]; then
             detect_first_color
-            line=$(echo -e "${COLOR_TITLE5}${BASH_REMATCH[1]}${first_color}")   # Title level 5
+            line=$(echo -e "${COLOR_TITLE5}${BASH_REMATCH[1]}${first_color}") 
         elif [[ "$line" =~ ^####[^#](.+)$ ]]; then
             detect_first_color
-            line=$(echo -e "${COLOR_TITLE4}${BASH_REMATCH[1]}${first_color}")   # Title level 4
+            line=$(echo -e "${COLOR_TITLE4}${BASH_REMATCH[1]}${first_color}")
         elif [[ "$line" =~ ^###[^#](.+)$ ]]; then
             detect_first_color
-            line=$(echo -e "${COLOR_TITLE3}${BASH_REMATCH[1]}${first_color}")   # Title level 3
+            line=$(echo -e "${COLOR_TITLE3}${BASH_REMATCH[1]}${first_color}")
         elif [[ "$line" =~ ^##[^#](.+)$ ]]; then
-            line=$(fill_background "${COLOR_TITLE2}" "${BASH_REMATCH[1]}")      # Title level 2
+            [[ "$NO_COLOR" != 1 || "$NO_FILL" != 1 ]] && line=$(fill_background "${COLOR_TITLE2}" "${BASH_REMATCH[1]}") 
+            [[ "$NO_COLOR" == 1 || "$NO_FILL" == 1 ]] && line=$(echo -e "${COLOR_TITLE2}" "${BASH_REMATCH[1]}") 
         elif [[ "$line" =~ ^#[^#](.+)$ ]]; then
-            line=$(centralize_text "${COLOR_TITLE1}" "${BASH_REMATCH[1]}")      # Title level 1
+            [[ "$NO_COLOR" != 1 || "$NO_CENTRALIZE" != 1 ]] && line=$(centralize_text "${COLOR_TITLE1}" "${BASH_REMATCH[1]}") 
+            [[ "$NO_COLOR" == 1 || "$NO_CENTRALIZE" == 1 ]] && line=$(echo -e "${COLOR_TITLE1}" "${BASH_REMATCH[1]}")      
         fi
 
         #if [[ "$line" =~ (.+)######[^#](.+)$ ]]; then
@@ -436,7 +468,7 @@ process_markdown() {
             line_shell  # Call line generation function
         # Handle Blockquotes
         elif [[ "$line" =~ ^\> ]]; then
-            echo -e "${COLOR_TITLE3}${line}${COLOR_RESET}"
+            echo -e "${COLOR_TITLE3}${line/\>/ \|}${COLOR_RESET}"
         # Block code
         elif [[ "$line" =~ ^\ {3} ]]; then
             echo -e "${COLOR_CODE}${line//\   /}${COLOR_RESET}"
@@ -467,6 +499,18 @@ main() {
             -h|--help)
                 show_help
                 ;;
+            -s|--simple)
+                export NO_COLOR=1
+                shift
+                ;;
+            -nc|--no-centralize)
+                export NO_CENTRALIZE=1
+                shift
+                ;;
+            -nf|--no-fill)
+                export NO_FILL=1
+                shift
+                ;;
             -nl|--no-less)
                 NO_LESS=${NO_LESS:-1}
                 shift
@@ -487,6 +531,8 @@ main() {
         esac
     done
     
+    define_colors
+
     # Check if file is provided
     if [ -z "$MARKDOWN_FILE" ]; then
         update_variables
