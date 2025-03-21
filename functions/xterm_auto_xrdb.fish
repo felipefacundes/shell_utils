26d67919d50325f37167e7c2e3cb5826040196bf
felipefacundes <felipe.facundes@gmail.com>
2025-03-21 19:13:06 -0300

    A Dynamic Collection of Shell Scripts with Educational Purpose

--- functions/xterm_auto_xrdb.fish
+++ functions/xterm_auto_xrdb.fish
@@ -1,19 +1,19 @@
-if test -z "$XTERM_VERSION" -a -f ~/.Xresources
+if test -z "$XTERM_VERSION"; and test -f ~/.Xresources
     if test ! -f ~/.xrdb_check.lock
-        cp -f ~/.Xresources ~/.xrdb_check.lock
-        xrdb -merge ~/.Xresources
+        cp -f ~/.Xresources ~/.xrdb_check.lock >/dev/null 2>&1
+        xrdb -merge ~/.Xresources >/dev/null 2>&1
     end
 
-    set md5sum_xresources (md5sum ~/.Xresources | awk '{ print $1 }')
-    set md5sum_xrdb_check (md5sum ~/.xrdb_check.lock | awk '{ print $1 }')
+    set md5sum_xresources (md5sum ~/.Xresources | awk '{ print $1 }' 2>/dev/null)
+    set md5sum_xrdb_check (md5sum ~/.xrdb_check.lock | awk '{ print $1 }' 2>/dev/null)
 
     if test "$md5sum_xresources" != "$md5sum_xrdb_check"
-        cp -f ~/.Xresources ~/.xrdb_check.lock
-        xrdb -merge ~/.Xresources
+        cp -f ~/.Xresources ~/.xrdb_check.lock >/dev/null 2>&1
+        xrdb -merge ~/.Xresources >/dev/null 2>&1
     end
 
     set -e md5sum_xresources
     set -e md5sum_xrdb_check
-else if test -n "$XTERM_VERSION" -a -f ~/.Xresources
-    xrdb -merge ~/.Xresources
+else if test -n "$XTERM_VERSION"; and test -f ~/.Xresources
+    xrdb -merge ~/.Xresources >/dev/null 2>&1
 end
--- functions/xterm_auto_xrdb.sh
+++ functions/xterm_auto_xrdb.sh
@@ -1,19 +1,19 @@
 if [[ -z "$XTERM_VERSION" ]] && [[ -f ~/.Xresources ]]; then
 	if [[ ! -f ~/.xrdb_check.lock ]]; then
-		cp -f ~/.Xresources ~/.xrdb_check.lock
-		xrdb -merge ~/.Xresources
+		cp -f ~/.Xresources ~/.xrdb_check.lock >/dev/null 2>&1
+		xrdb -merge ~/.Xresources >/dev/null 2>&1
 	fi
 
-	md5sum_xresources="$(md5sum ~/.Xresources | awk '{ print $1 }')"
-	md5sum_xrdb_check="$(md5sum ~/.xrdb_check.lock | awk '{ print $1 }')"
+	md5sum_xresources="$(md5sum ~/.Xresources | awk '{ print $1 }' 2>/dev/null)"
+	md5sum_xrdb_check="$(md5sum ~/.xrdb_check.lock | awk '{ print $1 }' 2>/dev/null)"
 
 	if [[ "$md5sum_xresources" != "$md5sum_xrdb_check" ]]; then
-		cp -f ~/.Xresources ~/.xrdb_check.lock
-		xrdb -merge ~/.Xresources
+		cp -f ~/.Xresources ~/.xrdb_check.lock >/dev/null 2>&1
+		xrdb -merge ~/.Xresources >/dev/null 2>&1
 	fi
 
 	unset md5sum_xresources
 	unset md5sum_xrdb_check
 elif [[ -n "$XTERM_VERSION" ]] && [[ -f ~/.Xresources ]]; then
-	xrdb -merge ~/.Xresources
+	xrdb -merge ~/.Xresources >/dev/null 2>&1
 fi
