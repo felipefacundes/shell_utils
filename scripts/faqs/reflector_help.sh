3050039a983431724a81e8da299666e3699c5990
felipefacundes <felipe.facundes@gmail.com>
2025-04-16 16:17:27 -0300

    A Dynamic Collection of Shell Scripts with Educational Purpose

--- scripts/faqs/reflector_help.sh
+++ scripts/faqs/reflector_help.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 cat <<'EOF'
-reflector -c Brazil --save /etc/pacman.d/mirrorlist
+sudo reflector -c Brazil --save /etc/pacman.d/mirrorlist
 
-reflector --verbose --age 8 --fastest 128 --latest 64 --number 32 --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector --verbose -c Brazil --latest 20 --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector --verbose -c Brazil --latest 20 --protocol https --sort rate --save /etc/pacman.d/mirrorlist
+
+sudo reflector --verbose -c Brazil --age 8 --fastest 128 --latest 64 --number 32 --sort rate --save /etc/pacman.d/mirrorlist
+sudo reflector --verbose -c Brazil --age 8 --fastest 128 --latest 64 --protocol https --number 32 --sort rate --save /etc/pacman.d/mirrorlist
 EOF
