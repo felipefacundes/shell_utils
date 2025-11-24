21193374a4bb667fe67c94ea69e454d028fc54d0
felipefacundes <felipe.facundes@gmail.com>
2025-11-23 22:13:27 -0300

    A Dynamic Collection of Shell Scripts with Educational Purpose

--- aliases/system/user.fish
+++ aliases/system/user.fish
@@ -1,3 +1,7 @@
 alias default_user='help default_user'
 alias my_user_default='default_user'
 alias user_default='default_user'
+
+alias clear_wine_shortcuts="rm -rf ~/.local/share/applications/*wine*"
+alias remove_wine_desktop_shortcuts="clear_wine_shortcuts"
+alias limpa_atalhos_do_wine="clear_wine_shortcuts"
