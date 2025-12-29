79b3bfc15312f5cd8ff8177d9b8b1bd5570e1d31
felipefacundes <felipe.facundes@gmail.com>
2025-12-29 19:10:36 -0300

    A Dynamic Collection of Shell Scripts with Educational Purpose

--- functions.fish
+++ functions.fish
@@ -1,4 +1,4 @@
-source $HOME/.shell_utils/functions/fzf.fish
 source $HOME/.shell_utils/functions/xterm_auto_xrdb.fish
+source $HOME/.shell_utils/functions/fzf.fish
 source $HOME/.shell_utils/functions/cd.fish
 source $HOME/.shell_utils/functions/notifications.fish
--- variables.fish
+++ variables.fish
@@ -1,4 +1,4 @@
+source $HOME/.shell_utils/variables/ffmpeg.fish
 source $HOME/.shell_utils/variables/environment/commons.fish
 source $HOME/.shell_utils/variables/environment/go.fish
 source $HOME/.shell_utils/variables/environment/user.fish
-source $HOME/.shell_utils/variables/ffmpeg.fish
